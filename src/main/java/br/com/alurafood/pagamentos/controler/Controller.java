76234df78b0daf52f455539b4f91d112723bbe72
Adailson <adailson65@gmail.com>
2024-09-08 00:50:01 -0300

    add circuit breaker

--- src/main/java/br/com/alurafood/pagamentos/controler/Controller.java
+++ src/main/java/br/com/alurafood/pagamentos/controler/Controller.java
@@ -3,6 +3,7 @@ package br.com.alurafood.pagamentos.controler;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -54,8 +55,13 @@ public class Controller {
     }
 
     @PatchMapping("/{id}/confirmar")
+    @CircuitBreaker(name = "atualizaPedido", fallbackMethod = "pagamentoAutorizadoComIntegracaoPendente")
     public void confirmarPagamento(@PathVariable @NotNull Long id){
         service.confirmarPagamento(id);
+    }
+
+    public void pagamentoAutorizadoComIntegracaoPendente(Long id, Exception e){
+        service.alteraStatus(id);
     }
 
 
--- src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
+++ src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -19,5 +19,4 @@ public class PagamentoDto {
     private Long pedidoId;
     private Long formaDePagamentoId;
 
-
 }
